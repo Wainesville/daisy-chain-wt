96a2c1b07d1464445209c1076709b09eff2662d1
Steven Waine <wainesville@gmail.com>
2024-12-04 21:39:48 -0800

    PORT.env

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 console.log('Type of authenticate:', typeof authenticate); // Add this line
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 const path = require('path');
 app.use(express.static(path.join(__dirname,'build')));
@@ -44,6 +44,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
