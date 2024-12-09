594baed00a55a54335a776ab2d9c04e58a8cc804
Steven Waine <wainesville@gmail.com>
2024-12-09 09:10:06 -0800

    add

--- backend/routes/watchlistRoutes.js
+++ backend/routes/watchlistRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { getWatchlist, addToWatchlist, getWatchlistByUserId, removeFromWatchlist, setCurrentlyWatching, setNextUp } = require('../controllers/watchlistController');
+const { getWatchlistByUserId, getWatchlist, addToWatchlist, removeFromWatchlist, setCurrentlyWatching, setNextUp } = require('../controllers/watchlistController');
 const authenticate = require('../middleware/authenticate'); // Authentication middleware
 
 const router = express.Router();
 
 router.get('/', authenticate, getWatchlist);
-router.post('/', authenticate, addToWatchlist);
-router.get('/:userId', authenticate, getWatchlistByUserId);
+router.post('/add', authenticate, addToWatchlist);
 router.delete('/remove/:movieId', authenticate, removeFromWatchlist);
 router.put('/currently-watching/:movieId', authenticate, setCurrentlyWatching);
 router.put('/next-up/:movieId', authenticate, setNextUp);
+router.get('/:userId', authenticate, getWatchlistByUserId); // Add the route to fetch the user's watchlist by user ID
 
 module.exports = router;
